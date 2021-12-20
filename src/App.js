8d01e739a51fc38ebcc26c853fec9632e060d3f7
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-20 15:04:04 -0600

    Moved Login functionality into Register.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './components/Navbar';
@@ -9,43 +9,8 @@ import Register from './components/Register';
 import Footer from './components/Footer';
 import Logout from './components/Logout';
 
-const initialFormValues = {
-  username: '',
-  email: '',
-  password: '',
-}
-
-const initialUsers = [];
 
 function App() {
-
-  const [users, setUsers] = useState(initialUsers);
-  const [formValues, setFormValues] = useState(initialFormValues);
-
-  const registerUser = newUser => {
-    axios.post('http://localhost:3000/api/login', newUser)
-      .then(res => {
-        console.log(res)
-        //setOrders([res.data, ...users])
-      }).catch(err => console.error(err))
-      .finally(() => setFormValues(initialFormValues))
-  }
-
-  const inputChange = (name, value) => {
-    setFormValues({
-      ...formValues,
-      [name]: value
-    })
-  }
-
-  const formSubmit = () => {
-    const newUser = {
-      name: formValues.name.trim(),
-      email: formValues.email.trim(),
-      password: formValues.password.trim(),
-    }
-    registerUser(newUser);
-  }
 
   return (
     <div className="App">
@@ -55,7 +20,7 @@ function App() {
         <Routes>
           <Route path="/logout" element={<Logout />} />
           <Route exact path='/login' element={<Login />} />
-          <Route exact path='/register' element={<Register values={formValues} change={inputChange} submit={formSubmit}/>}  />
+          <Route exact path='/register' element={<Register />}  />
           <Route exact path='/' element={<Home />} />
         </Routes>
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterForm from './RegisterForm';
+import axios from 'axios';
 
-function Register(props){
+const initialFormValues = {
+    username: '',
+    email: '',
+    password: '',
+}
+  
+const initialUsers = [];
 
-    const { values, change, submit } = props;
+
+function Register(){
+    const [users, setUsers] = useState(initialUsers);
+    const [formValues, setFormValues] = useState(initialFormValues);
+
+    const registerUser = newUser => {
+        axios.post('http://localhost:3000/api/login', newUser)
+        .then(res => {
+            console.log(res)
+            //setOrders([res.data, ...users])
+        }).catch(err => console.error(err))
+        .finally(() => setFormValues(initialFormValues))
+    }
+
+    const inputChange = (name, value) => {
+        setFormValues({
+        ...formValues,
+        [name]: value
+        })
+    }
+
+    const formSubmit = () => {
+        const newUser = {
+        name: formValues.name.trim(),
+        email: formValues.email.trim(),
+        password: formValues.password.trim(),
+        }
+        registerUser(newUser);
+    }
+
+    
 
     return(
         <>
             <h1>Register below</h1>
-            <RegisterForm values={values} change={change} submit={submit}/>
+            <RegisterForm  values={formValues} change={inputChange} submit={formSubmit}/>
         </>
     )
 }
