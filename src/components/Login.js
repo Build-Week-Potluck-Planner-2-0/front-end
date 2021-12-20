55582fde3d3c185adaf6e20e477e8c4ca8cbdd46
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-20 12:08:58 -0600

    Started Routes

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+function Login(){
+
+    return(
+        <>
+            <h1>Login below</h1>
+            <LoginForm />
+        </>
+    )
+}
+
+
+export default Login;
